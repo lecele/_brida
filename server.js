f0c1d1a6765a08f8614965063f977ae620dfe2a1
Leonardo Leite <lecele@gmail.com>
2025-12-10 21:29:56 -0300

    fix: update tunnel url to latest

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 // Configuração
 const app = express();
 const PORT = 3001;
-const N8N_URL = 'https://eighty-sloths-fix.loca.lt/webhook/76481988-7d5a-4a82-a6ef-bffad0b029c3/chat';
+const N8N_URL = 'https://easy-towns-happen.loca.lt/webhook/76481988-7d5a-4a82-a6ef-bffad0b029c3/chat';
 
 // Middleware
 app.use(cors());
